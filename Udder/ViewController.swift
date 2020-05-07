59f2f1cbade68dda29238aa0caf633c8a743e667
bad munkey <badmunkey>
2020-05-07 01:27:16 -0700

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by munkey on 5/7/20.
+//  Copyright © 2020 munkeys in barrels. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
